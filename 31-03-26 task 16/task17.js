f5815da5921ddb7015afff5450486b7b36e09dd3
PRITHIV0418 <prithivpsa@gmail.com>
2026-04-01 06:59:56 +0530

    Add files via upload

---
+++ 31-03-26 task 16/task17.js
@@ -0,0 +1,14 @@
+var username = "John";
+let score = 50;
+const passMark = 40;
+
+let isPassed = score >= passMark;
+
+console.log(username);
+console.log("Passed:", isPassed);
+
+console.log(score + 10);
+console.log(score == "50");
+console.log(score === "50");
+console.log(isPassed && true);
+console.log(isPassed || false);
