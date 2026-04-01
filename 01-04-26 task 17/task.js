1935c087e5db19f13b4fc06265d701b88b42a599
PRITHIV0418 <prithivpsa@gmail.com>
2026-04-01 20:25:15 +0530

    Add files via upload

---
+++ 01-04-26 task 17/task.js
@@ -0,0 +1,37 @@
+let num = 5;
+
+if (num > 0) {
+    console.log("Positive");
+} else if (num < 0) {
+    console.log("Negative");
+} else {
+    console.log("Zero");
+}
+//task 2
+let day = 3;
+
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+}
